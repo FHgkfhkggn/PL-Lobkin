14203505cacdbe18cc8c1c593a320da7229dac38
FHgkfhkggn <dany.mostowoi@gmail.com>
2024-12-04 18:04:04 +0300

    Update пр9.2.py

--- пр9.2.py
+++ пр9.2.py
@@ -1,14 +1,7 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
-def calculate_expression(x, n):
-    if n == 0:
-        return 1
-    else:
-        return (x ** n) / factorial(n)
-x = int(input("Введите натуральное число X: "))
-n = int(input("Введите натуральное число n: "))
-result = calculate_expression(x, n)
-print(f"Результат выражения {x}^{n}/{n}! равен: {result}")
+from math import 
+print('№1')
+def f(x,n):
+return (x**n)/factorial(n)
+x = int(input())
+y = int(input())
+print(f(x,y))
